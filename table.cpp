894708a4bd420429937951b2b8bef7bf45cbcb52
Pavan-manikanta29 <pavanmanikantayadla123@gmail.com>
2025-07-24 10:46:03 +0000

    Create table.cpp

---
+++ table.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void table(int a){
+	for(int i=1;i<=10;i++){
+		cout<<a<<"*"<<i<<"="<<a*i<<endl;
+		cout<<endl;
+	}
+}
+
+/*
+#include<iostream>
+using namespace std;
+void table(int a);
+int main(){
+int num;
+cout<<"Enter which table do you want";
+cin>>num;
+	table(2);
+	table(4);
+	table(num);
+	cout<<"data"
+	return 0;
+}
+*/
